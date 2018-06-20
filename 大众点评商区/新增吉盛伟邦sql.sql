318e0265faebda85b1ee6b2f2fdccb8da4059c57
chan <6442742@qq.com>
2018-06-20 11:35:10 +0800

    完成卖场加入商圈

---
+++ 大众点评商区/新增吉盛伟邦sql.sql
@@ -0,0 +1,7 @@
+INSERT INTO `feigo_regions` (`id`,`name`,`parent_id`,`level_type`, `merger_name`, `lng`, `lat`) VALUES (220105005,'吉盛伟邦',220105,4,'中国,吉林省,长春市,二道区,吉盛伟邦',125.361391,43.896986);  #  四通路168号吉盛伟邦5层====吉盛伟邦(长春店) 
+INSERT INTO `feigo_regions` (`id`,`name`,`parent_id`,`level_type`, `merger_name`, `lng`, `lat`) VALUES (310105008,'吉盛伟邦',310105,4,'中国,上海,上海市,长宁区,吉盛伟邦',121.402446,31.203754);  #  娄山关路75号====吉盛伟邦进口家俬馆 
+INSERT INTO `feigo_regions` (`id`,`name`,`parent_id`,`level_type`, `merger_name`, `lng`, `lat`) VALUES (310118008,'吉盛伟邦',310118,4,'中国,上海,上海市,青浦区,吉盛伟邦',121.22968,31.162127);  #  嘉松中路5369号====吉盛伟邦国际家具村 
+INSERT INTO `feigo_regions` (`id`,`name`,`parent_id`,`level_type`, `merger_name`, `lng`, `lat`) VALUES (330302047,'吉盛伟邦',330302,4,'中国,浙江省,温州市,鹿城区,吉盛伟邦',120.628366,28.013613);  #  黄龙商贸城中心街176号====吉盛纬邦 
+INSERT INTO `feigo_regions` (`id`,`name`,`parent_id`,`level_type`, `merger_name`, `lng`, `lat`) VALUES (440105009,'吉盛伟邦',440105,4,'中国,广东省,广州市,海珠区,吉盛伟邦',113.367571,23.097207);  #  新港东路1000号====吉盛伟邦(琶洲店) 
+INSERT INTO `feigo_regions` (`id`,`name`,`parent_id`,`level_type`, `merger_name`, `lng`, `lat`) VALUES (440113014,'吉盛伟邦',440113,4,'中国,广东省,广州市,番禺区,吉盛伟邦',113.342237,23.008413);  #  迎宾大道万博中心吉盛伟邦家居博览中心(D馆111-112)====吉盛伟邦国际馆 
+INSERT INTO `feigo_regions` (`id`,`name`,`parent_id`,`level_type`, `merger_name`, `lng`, `lat`) VALUES (440604016,'吉盛伟邦',440604,4,'中国,广东省,佛山市,禅城区,吉盛伟邦',113.029455,23.001564);  #  季华西路128号永利达广场2座X101号====上海吉盛伟邦环球家居品牌管理有限公司(佛山禅城分公司) 
